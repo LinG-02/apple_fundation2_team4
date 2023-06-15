412ecf3787188af02e684d066027e3ecb2280220
AntonioPerez888 <antonioperezz2091@gmail.com>
2023-06-15 12:19:47 +1000

    Edited prototype

--- Error Nil/ChallengePage.swift
+++ Error Nil/ChallengePage.swift
@@ -51,7 +51,7 @@ struct ChallengePage: View {
 //                        Text("Welcome to the Challenge stage! In this stage, you will transform your essential question from the previous stage into a powerful call to action statement.")
 //                            .padding(.bottom)
                         
-                        Text("Do you still remember your essential question? Retrieve it from the \"Big Idea\" stage and write it down below.")
+                        Text("Do you still remember your Essential Question? Retrieve it from the \"Big Idea\" stage and write it down below.")
                             .padding(.bottom)
                         
                         ZStack(alignment: .topLeading) {
@@ -63,7 +63,7 @@ struct ChallengePage: View {
                                 .lineLimit(5)
                         }
                         
-                        Text("Now, using your essential question as a starting point, let's generate some challenge statements. Please list them below.")
+                        Text("Now, using your Essential Question as a starting point, let's generate some Challenge Statements. Please list them below.")
                             .padding(.top)
                         
                         Text("(*You may find the tips in the \"info\" tab helpful.)")
@@ -81,7 +81,7 @@ struct ChallengePage: View {
                         }
                         
                         Group {
-                            Text("Read through the challenge statements and select your favorite one write it down in the box below.")
+                            Text("Read through the Challenge Statements and select your favourite one write it down in the box below.")
                                 .padding(.top)
                             
                             ZStack(alignment: .topLeading) {
@@ -182,7 +182,7 @@ struct ChallengePopupView: View {
                             .bold()
                             .padding(.top, 20)
                         
-                        Text("You can utilize the Challenge Mindmapping technique to efficiently generate challenge statements around your essential question. This technique will assist you in the process of brainstorming and exploring various possibilities.")
+                        Text("You can utilise the Challenge Mindmapping technique to efficiently generate Challenge Statements around your Essential Question. This technique will assist you in the process of brainstorming and exploring various possibilities.")
                             .font(.system(size: 16))
                         
                         Text("STEP 1")
@@ -200,7 +200,7 @@ struct ChallengePopupView: View {
                             .font(.system(size: 17))
                             .bold()
                         
-                        Text("If you are in a team, you can vote for your favorite Challenge Statement with your team to determine your team's challenge statement. If you are working individually, carefully review your listed challenge statements and choose your favorite one.")
+                        Text("If you are in a team, you can vote for your favorite Challenge Statement with your team to determine your team's Challenge Statement. If you are working individually, carefully review your listed Challenge Statements and choose your favourite one.")
                             .font(.system(size: 16))
                         
                         Image("challengemindmap2")
